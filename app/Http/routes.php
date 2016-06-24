c7ce03b2c0e9fa5b35aa5037eb0ba4505a8eb1c6
netoudi <netoudi@rocketmail.com>
2016-06-24 12:19:25 -0300

    Fase - Criação das rotas.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/categories', 'AdminCategoriesController@index');
+Route::group(['prefix' => 'admin'], function () {
 
-Route::get('/admin/products', 'AdminProductsController@index');
+    Route::group(['prefix' => 'categories'], function () {
+        Route::get('/', 'AdminCategoriesController@index');
+        Route::get('/create', 'AdminCategoriesController@create');
+        Route::post('/store', 'AdminCategoriesController@store');
+        Route::get('/show/{id}', 'AdminCategoriesController@show');
+        Route::get('/edit/{id}', 'AdminCategoriesController@edit');
+        Route::put('/update/{id}', 'AdminCategoriesController@update');
+        Route::delete('/destroy/{id}', 'AdminCategoriesController@destroy');
+    });
+
+    Route::group(['prefix' => 'products'], function () {
+        Route::get('/', 'AdminProductsController@index');
+        Route::get('/create', 'AdminProductsController@create');
+        Route::post('/store', 'AdminProductsController@store');
+        Route::get('/show/{id}', 'AdminProductsController@show');
+        Route::get('/edit/{id}', 'AdminProductsController@edit');
+        Route::put('/update/{id}', 'AdminProductsController@update');
+        Route::delete('/destroy/{id}', 'AdminProductsController@destroy');
+    });
+
+});
